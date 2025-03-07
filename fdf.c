3fd0f65df7d4bc30f1ebff94bfd5676a896c148d
An Tran <atran@c1r3p5.hive.fi>
2025-03-07 18:02:18 +0200

    add mlx and points rendering

--- fdf.c
+++ fdf.c
@@ -6,33 +6,45 @@
 /*   By: atran <atran@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 10:04:17 by atran             #+#    #+#             */
-/*   Updated: 2025/02/18 17:33:50 by atran            ###   ########.fr       */
+/*   Updated: 2025/03/07 17:54:00 by atran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-/* void	ft_print_map(char ***map)
+void	render_points(t_point **grid, mlx_image_t *image)
 {
-	int	i;
-	int	j;
-
-	i = -1;
-	while (map[++i])
-	{
-		j = -1;
-		while (map[i][++j])
-			ft_printf("%s", map[i][j]);
-	}
-} */
-
-int	main(int argc, char **argv)
-{
-	t_point	**grid;
+	double	radians;
 	int		i;
 	int		j;
 
 	i = 0;
+	radians = 30 * (M_PI / 180);
+	while (grid[i])
+	{
+		j = 0;
+		while (grid[i][j].color)
+		{
+			grid[i][j].x = i * cos(radians) - j * cos(radians);
+			grid[i][j].y = i * sin(radians) + j * sin(radians)
+				- grid[i][j].value;
+			mlx_put_pixel(image, grid[i][j].x + WIDTH / 2, grid[i][j].y + HEIGHT
+				/ 2, grid[i][j].color);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_point		**grid;
+	mlx_t		*mlx;
+	mlx_image_t	*image;
+
+	/* int			i;
+	int			j; */
+	/* i = 0; */
 	if (argc != 2)
 		return (0);
 	if (file_exist_or_empty(argv[1]) < 0)
@@ -40,26 +52,25 @@ int	main(int argc, char **argv)
 	grid = create_grid(argv[1]);
 	if (!grid)
 		return (0);
-	while (grid[i])
+	mlx = mlx_init(WIDTH, HEIGHT, "FdF", true);
+	if (!mlx)
+		return (0);
+	image = mlx_new_image(mlx, WIDTH, HEIGHT);
+	render_points(grid, image);
+	mlx_image_to_window(mlx, image, 0, 0);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	/* while (grid[i])
 	{
 		j = 0;
 		while (grid[i][j].color)
 		{
-			ft_printf(" %d, %s", grid[i][j].value, grid[i][j].color);
+			ft_printf(" // %d, %s, %d, %d", grid[i][j].value, grid[i][j].color,
+				grid[i][j].x, grid[i][j].y);
 			j++;
 		}
 		ft_printf("\n");
 		i++;
-	}
+	} */
 	ft_free_grid(grid);
 }
-/*Check input:
-- File exist and can be opened.
-- Check if file can be read.
-- Handle empty file
-- File doesn't end with '\n'
-- File need to end with .fdf
-- Each row need to have same number of value
-- Value need to be a valid number (not overflow), and can be turned into number
-- file need to have nly number, space, new line and color code.
-- */
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: atran <atran@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 10:04:47 by atran             #+#    #+#             */
-/*   Updated: 2025/02/18 17:31:18 by atran            ###   ########.fr       */
+/*   Updated: 2025/03/07 17:42:36 by atran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,23 @@
 # include "MLX42/include/MLX42/MLX42.h"
 # include <errno.h>
 # include <fcntl.h>
+# include <math.h>
 # include <string.h>
 # include <unistd.h>
+
+# ifndef M_PI
+#  define M_PI 3.14159265358979323846
+# endif
+
+# define WIDTH 800
+# define HEIGHT 600
 
 typedef struct s_point
 {
 	int		value;
 	char	*color;
+	int		x;
+	int		y;
 }			t_point;
 
 int			ft_printf(const char *str, ...);
